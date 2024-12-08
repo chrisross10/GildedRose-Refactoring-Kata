33c7c1234c9857f1768d94b57044b1eff3bc901a
Chris Ross <info.cjrdigital@gmail.com>
2024-12-08 15:23:04 +0000

    Pull up conditional for isAgedBrie
    Clean up code

--- csharp.xUnit/GildedRose/Item.cs
+++ csharp.xUnit/GildedRose/Item.cs
@@ -9,59 +9,72 @@ public class Item
     public void UpdateItem()
     {
         var isAgedBrie = Name == "Aged Brie";
-        
-        if (isAgedBrie || Name == "Backstage passes to a TAFKAL80ETC concert")
+
+        if (isAgedBrie)
         {
             if (Quality < 50)
             {
                 Quality += 1;
-
-                if (Name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (SellIn < 11)
-                    {
-                        if (Quality < 50)
-                        {
-                            Quality += 1;
-                        }
-                    }
-
-                    if (SellIn < 6)
-                    {
-                        if (Quality < 50)
-                        {
-                            Quality += 1;
-                        }
-                    }
-                }
             }
-        }
-        else
-        {
-            if (Quality > 0)
+
+            if (Name != "Sulfuras, Hand of Ragnaros")
             {
-                if (Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Quality -= 1;
-                }
+                SellIn -= 1;
             }
-        }
 
-        if (Name != "Sulfuras, Hand of Ragnaros")
-        {
-            SellIn -= 1;
-        }
-
-        if (SellIn < 0)
-        {
-            if (isAgedBrie)
+            if (SellIn < 0)
             {
                 if (Quality < 50)
                 {
                     Quality += 1;
                 }
             }
+        }
+        else
+        {
+            if (Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (Quality < 50)
+                {
+                    Quality += 1;
+
+                    if (Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (SellIn < 11)
+                        {
+                            if (Quality < 50)
+                            {
+                                Quality += 1;
+                            }
+                        }
+
+                        if (SellIn < 6)
+                        {
+                            if (Quality < 50)
+                            {
+                                Quality += 1;
+                            }
+                        }
+                    }
+                }
+            }
             else
+            {
+                if (Quality > 0)
+                {
+                    if (Name != "Sulfuras, Hand of Ragnaros")
+                    {
+                        Quality -= 1;
+                    }
+                }
+            }
+
+            if (Name != "Sulfuras, Hand of Ragnaros")
+            {
+                SellIn -= 1;
+            }
+
+            if (SellIn < 0)
             {
                 if (Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
