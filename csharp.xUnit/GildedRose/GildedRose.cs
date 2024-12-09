0e5308d683e8d99e4ce17993810a049b10752825
Chris Ross <info.cjrdigital@gmail.com>
2024-12-09 14:45:00 +0000

    Decouple the item creating logic
    This can now be injected in by a calling class

--- csharp.xUnit/GildedRose/GildedRose.cs
+++ csharp.xUnit/GildedRose/GildedRose.cs
@@ -1,23 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace GildedRoseKata;
 
 public class GildedRose
 {
     private readonly List<UpdatableItem> _updatableItems;
+    private static readonly Dictionary<string, Func<Item, UpdatableItem>> ItemCreators = new()
+    {
+        { "Aged Brie", item => new AgedBrieItem(item) },
+        { "Backstage passes to a TAFKAL80ETC concert", item => new BackstagePassesItem(item) },
+        { "Sulfuras, Hand of Ragnaros", item => new SulfurasItem(item) }
+    };
 
     public GildedRose(IList<Item> Items)
     {
         _updatableItems = new List<UpdatableItem>();
         foreach (var item in Items)
         {
-            _updatableItems.Add(item.Name switch
-            {
-                "Aged Brie" => new AgedBrieItem(item),
-                "Backstage passes to a TAFKAL80ETC concert" => new BackstagePassesItem(item),
-                "Sulfuras, Hand of Ragnaros" => new SulfurasItem(item),
-                _ => new DefaultItem(item)
-            });
+            _updatableItems.Add(ItemCreators.TryGetValue(item.Name, out var creator)
+                ? creator(item)
+                : new DefaultItem(item));
         }
     }
 
